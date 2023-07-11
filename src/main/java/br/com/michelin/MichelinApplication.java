8c517832baae20cdecedd548d5ce77f8cef5d13e
Caua-pereira-santos <cauap600@gmail.com>
2023-07-11 16:50:49 -0300

    Testando Hospedagem Railway

--- src/main/java/br/com/michelin/MichelinApplication.java
+++ src/main/java/br/com/michelin/MichelinApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MichelinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MichelinApplication.class, args);
-	}
+	public String PORT = System.getenv("PORT");
+	public static void main(String[] args) {SpringApplication.run(MichelinApplication.class, args);}
 
 }
